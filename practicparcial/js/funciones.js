a5eb5bff64a347a60b837187e3f87a9c75da2b18
ZabalaDaniel <dzabala544@gmail.com>
2024-05-31 20:42:22 -0300

    practica parcial

--- practicparcial/js/funciones.js
+++ practicparcial/js/funciones.js
@@ -1,25 +1,57 @@
 
 function searchUser(){
-    fetch(`personas.json`)
+
+     let local=document.getElementById("local").checked;
+     let internet= document.getElementById("internet").checked;
+
+ document.getElementById("mujeres").innerHTML="";
+ document.getElementById("hombres").innerHTML="";
+ document.getElementById("contador").innerHTML="";
+ document.getElementById("contador2").innerHTML="";
+ document.getElementById("contador3").innerHTML="";
+  let url;
+
+
+     if (!local && !internet) {
+          alert(`debe seleccionar una opcion`)
+        return false
+        
+     } else {
+        
+ }
+  if (local) {
+   url=`personas.json`
+    
+   } else {
+         url=`https://randomuser.me/api/?results=10`
+    }
+  
+    fetch(url)
     .then(response => response.json()) 
     .then(data => {
         console.log(data["results"]); 
 
         let array = []; 
         let array2 = []; 
+        let array3=[];
         let mujeres = 0; 
         let varones = 0; 
 
         data["results"].forEach(user => {
             if (user.gender === "female") {
-                array.push(user.name.first + " " + user.name.last);
+                array.push(user.name.first + " " + user.name.last+" "+user.dob.age);
                 mujeres += 1;
             }
             else {
-                array2.push(user.name.first + " " + user.name.last);
+                array2.push(user.name.first + " " + user.name.last+" "+user.dob.age);
                 varones += 1;   
             }
         });
+
+
+        array3=array.concat(array2)
+        console.log(array3)
+
 
         // Obtiene los contenedores para las mujeres y los hombres
         const mujeresDiv = document.getElementById("mujeres");
